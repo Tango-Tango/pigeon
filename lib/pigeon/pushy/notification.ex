ff0871bcf9a5bc822c07ea9d59cad38e7803c1db
Gary Rennie <gazler@gmail.com>
2025-01-22 10:01:54 +0000

    feat(Pigeon.Pushy): use protocols for encoding body and headers
    The `Pigeon.Configurable` protocol is now used to generate the headers
    and payload from the notification. This is more in line with how the
    other adapters work, and allows testing the generated payloads
    separately from sending the notification.

--- lib/pigeon/pushy/notification.ex
+++ lib/pigeon/pushy/notification.ex
@@ -214,3 +214,37 @@ defmodule Pigeon.Pushy.Notification do
     %{notification | collapse_key: collapse_key}
   end
 end
+
+defimpl Pigeon.Encodable, for: Pigeon.Pushy.Notification do
+  def binary_payload(notif) do
+    encode_requests(notif)
+  end
+
+  @doc false
+  def encode_requests(notif) do
+    %{}
+    |> encode_to(notif.to)
+    |> encode_data(notif.data)
+    |> maybe_encode_attr("time_to_live", notif.time_to_live)
+    |> maybe_encode_attr("content_available", notif.content_available)
+    |> maybe_encode_attr("mutable_content", notif.mutable_content)
+    |> maybe_encode_attr("notification", notif.notification)
+    |> maybe_encode_attr("schedule", notif.schedule)
+    |> maybe_encode_attr("collapse_key", notif.collapse_key)
+    |> Pigeon.json_library().encode!()
+  end
+
+  defp encode_to(map, value) do
+    Map.put(map, "to", value)
+  end
+
+  defp encode_data(map, value) do
+    Map.put(map, "data", value)
+  end
+
+  defp maybe_encode_attr(map, _key, nil), do: map
+
+  defp maybe_encode_attr(map, key, val) do
+    Map.put(map, key, val)
+  end
+end
